a060c28ac477b2ed576c89241861edb3a3aee7fc
ChirayuGupta2003 <chirayugupta2003@gmail.com>
2023-04-18 22:39:50 +0530

    edited main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import os
 import cv2
 import gdown
+import PIL
 
 
 app = flask.Flask(__name__)
@@ -50,30 +51,31 @@ def home():
     return "Working"
 
 
-if __name__ == "__main__":
 
-    model = InceptionV3(weights='imagenet', include_top=False,
-                        input_shape=(228, 228, 3), pooling='avg')
+model = InceptionV3(weights='imagenet', include_top=False,
+                    input_shape=(228, 228, 3), pooling='avg')
 
-    for layer in model.layers:
-        layer.trainable = False
+for layer in model.layers:
+    layer.trainable = False
 
-    input = model.output
-    input = BatchNormalization(axis=-1)(input)
-    input = Dense(1024, activation='relu')(input)
-    input = Dropout(0.3)(input)
-    output = Dense(20, activation='softmax')(input)
-    m = Model(inputs=model.input, outputs=output)
-    m.compile(optimizer='adam', metrics=[
-              'accuracy'], loss='categorical_crossentropy')
+input = model.output
+input = BatchNormalization(axis=-1)(input)
+input = Dense(1024, activation='relu')(input)
+input = Dropout(0.3)(input)
+output = Dense(20, activation='softmax')(input)
+m = Model(inputs=model.input, outputs=output)
+m.compile(optimizer='adam', metrics=[
+            'accuracy'], loss='categorical_crossentropy')
 
-    if not os.path.isfile("food_recognition_inceptionV3.h5"):
-        gdown.download("https://drive.google.com/file/d/1Y5ALEolZrlyYYRx9HEIC39aU4eBV2XjF/view?usp=share_link",
-                       "food_recognition_inceptionV3.h5", quiet=False, fuzzy=True)
+if not os.path.isfile("food_recognition_inceptionV3.h5"):
+    gdown.download("https://drive.google.com/file/d/1Y5ALEolZrlyYYRx9HEIC39aU4eBV2XjF/view?usp=share_link",
+                    "food_recognition_inceptionV3.h5", quiet=False, fuzzy=True)
 
-    m.load_weights("food_recognition_inceptionV3.h5")
+m.load_weights("food_recognition_inceptionV3.h5")
 
-    classes = ['burger', 'butter_naan', 'chai', 'chapati', 'chole_bhature', 'dal_makhani', 'dhokla', 'fried_rice', 'idli', 'jalebi',
-               'kaathi_rolls', 'kadai_paneer', 'kulfi', 'masala_dosa', 'momos', 'paani_puri', 'pakode', 'pav_bhaji', 'pizza', 'samosa']
+classes = ['burger', 'butter_naan', 'chai', 'chapati', 'chole_bhature', 'dal_makhani', 'dhokla', 'fried_rice', 'idli', 'jalebi',
+            'kaathi_rolls', 'kadai_paneer', 'kulfi', 'masala_dosa', 'momos', 'paani_puri', 'pakode', 'pav_bhaji', 'pizza', 'samosa']
 
-    app.run(debug=True, host="0.0.0.0")
+
+if __name__=="__main__":
+    app.run(port=5000)
