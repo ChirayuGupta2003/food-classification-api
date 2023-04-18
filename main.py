a5276ac85bbe457b7795d1519883b0e726cf2343
ChirayuGupta2003 <chirayugupta2003@gmail.com>
2023-04-18 22:22:00 +0530

    corrected preprocessing algo

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import flask_cors
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 import cv2
 import gdown
 
-    
+
 app = flask.Flask(__name__)
 cors = flask_cors.CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-def predict_image(img_array):
-
+def predict_image(filename, model):
+    img_ = image.load_img(filename, target_size=(228, 228))
+    img_array = image.img_to_array(img_)
     img_processed = np.expand_dims(img_array, axis=0)
-    img_processed = img_processed / 255
+    img_processed /= 255.
 
-    prediction = m.predict(img_processed)
+    prediction = model.predict(img_processed)
 
     index = np.argmax(prediction)
-    confidence = prediction[0][index]
 
-    return index, confidence
+    return index, prediction[0][index]
 
 
 @app.route("/")
@@ -35,15 +36,17 @@ def home():
 
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-    img = cv2.resize(img, (228, 228), interpolation=cv2.INTER_NEAREST)
+    cv2.imwrite("image.jpg", img)
 
-    index, confidence = predict_image(img)
+    index, confidence = predict_image("image.jpg", m)
 
     data = {"class": classes[index], "confidence": str(confidence)}
     print(data)
 
+    os.remove('image.jpg')
+
     if data:
-        return flask.jsonify(data)
+        return data
     return "Working"
 
 
@@ -73,4 +76,4 @@ if __name__ == "__main__":
     classes = ['burger', 'butter_naan', 'chai', 'chapati', 'chole_bhature', 'dal_makhani', 'dhokla', 'fried_rice', 'idli', 'jalebi',
                'kaathi_rolls', 'kadai_paneer', 'kulfi', 'masala_dosa', 'momos', 'paani_puri', 'pakode', 'pav_bhaji', 'pizza', 'samosa']
 
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
