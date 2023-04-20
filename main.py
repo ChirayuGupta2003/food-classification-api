e7d634a88e4e8f30dcbfb00d5f193ab4eeb45cd8
ChirayuGupta2003 <chirayugupta2003@gmail.com>
2023-04-20 12:56:47 +0530

    minor fixes

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ classes = ['burger', 'butter_naan', 'chai', 'chapati', 'chole_bhature', 'dal_mak
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
